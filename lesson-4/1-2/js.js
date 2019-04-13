02d2b79b7ec7fb99b6886701e1723b3d0b35ffac
ncowboy <latentpickuper@gmail.com>
2019-04-13 15:13:31 +0300

    done

--- lesson-4/1-2/js.js
+++ lesson-4/1-2/js.js
@@ -1,23 +1,25 @@
 class QuotesSwitcher {
-  constructor(blockClass, buttonClass, regexp, newQuote) {
-    this.blockClass = blockClass;
-    this.buttonClass = buttonClass;
-    this.regexp = regexp;
-    this.newQuote = newQuote;
-    this.init();
-  }
-  init() {
-    document.getElementById('buttons').addEventListener('click', (e) =>{
-      if(e.target.classList[1] === this.buttonClass) {
-        this.replace();
-      }
-    });
-  }
+    constructor(blockClass, buttonClass, regexp, newQuote) {
+        this.blockClass = blockClass;
+        this.buttonClass = buttonClass;
+        this.regexp = regexp;
+        this.newQuote = newQuote;
+        this.init();
+    }
 
-  replace(){
-    const block = document.querySelector(`.${this.blockClass}`);
-    block.innerHTML = block.innerHTML.replace(this.regexp, this.newQuote);
-  }
+    init() {
+        document.getElementById('buttons').addEventListener('click', (e) => {
+            if (e.target.classList[1] === this.buttonClass) {
+                this.replace();
+            }
+        });
+    }
+
+    replace() {
+        const block = document.querySelector(`.${this.blockClass}`);
+        block.innerHTML = block.innerHTML.replace(this.regexp, this.newQuote);
+    }
 }
+
 const switch1 = new QuotesSwitcher('big-text', 'btn-success', /'/g, '"');
 const switch2 = new QuotesSwitcher('big-text', 'btn-primary', /(\B')|('\B)/g, '"');
--- lesson-4/3/js.js
+++ lesson-4/3/js.js
@@ -1,73 +1,69 @@
 class FormValidator {
-  constructor(formSelector) {
-    this.formSelector = formSelector;
-    this.rules = {
-      name: /^[A-zА-яё]+$/i,
-      phone: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
-      email: /^[\w\.\-_]+@\w+\.\w{2,3}$/i
-    };
-    this.errors = {
-      name: 'Имя содержит только буквы',
-      phone: 'Телефон должен быть в формате +7(000)000-0000',
-      email: 'Введите корректный email'
-    };
-    this.isValid = false;
-    this._init();
-  }
+    constructor(formSelector) {
+        this.formSelector = formSelector;
+        this.rules = {
+            name: /^[A-zА-яё]+$/i,
+            phone: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
+            email: /^[\w\.\-_]+@\w+\.\w{2,3}$/i
+        };
+        this.errors = {
+            name: 'Имя содержит только буквы',
+            phone: 'Телефон должен быть в формате +7(000)000-0000',
+            email: 'Введите корректный email'
+        };
+        this.isValid = false;
+        this._init();
+    }
 
-  _init() {
-    const form = document.querySelector(this.formSelector);
-    form.addEventListener('submit', e => {
-      console.log(this._validateFields(form));
-      // if(!(this._validateFields(form) && this.isValid)){
-      //   e.preventDefault();
-      // }
-    });
+    _init() {
+        const form = document.querySelector(this.formSelector);
+        form.addEventListener('submit', e => {
+            this._validateFields(form);
+            if (!this.isValid) {
+                e.preventDefault();
+            }
+        });
+    }
 
+    _validateFields(formEl) {
+        const fields = [...formEl.getElementsByTagName('INPUT')];
+        fields.forEach(field => {
+            this._watchField(field);
+            if (this._validateField(field)) {
+                FormValidator._handleSuccess(field);
+                this.isValid = true;
+            } else {
+                this._handleError(field);
+                this.isValid = false;
+            }
+        });
+    }
 
-  }
+    _validateField(field) {
+        return this.rules[field.name].test(field.value);
+    }
 
-  _validateFields(formEl) {
-    const fields = [...formEl.getElementsByTagName('INPUT')];
-    fields.forEach(field => {
-      this._watchField(field);
-      if (this._validateField(field)) {
-        this._handleSuccess(field);
-        //this.isValid = true;
-        return true;
-      } else {
-        this._handleError(field);
-        //this.isValid = false;
-        return false;
-      }
-    });
-  }
+    static _handleSuccess(field) {
+        field.parentElement.classList.remove('has-error');
+        field.nextElementSibling.innerHTML = '';
+        field.parentElement.classList.add('has-success');
+    }
 
-  _validateField(field) {
-    return this.rules[field.name].test(field.value);
-  }
+    _handleError(field) {
+        field.parentElement.classList.remove('has-success');
+        field.parentElement.classList.add('has-error');
+        field.nextElementSibling.innerHTML = this.errors[field.name];
+    }
 
-  _handleSuccess(field) {
-    field.parentElement.classList.remove('has-error');
-    field.nextElementSibling.innerHTML = '';
-    field.parentElement.classList.add('has-success');
-  }
-
-  _handleError(field) {
-    field.parentElement.classList.remove('has-success');
-    field.parentElement.classList.add('has-error');
-    field.nextElementSibling.innerHTML = this.errors[field.name];
-  }
-
-  _watchField(field) {
-    field.addEventListener('input', () => {
-      if (this._validateField(field)) {
-        this._handleSuccess(field);
-      } else {
-        this._handleError(field);
-      }
-    });
-  }
+    _watchField(field) {
+        field.addEventListener('input', () => {
+            if (this._validateField(field)) {
+                FormValidator._handleSuccess(field);
+            } else {
+                this._handleError(field);
+            }
+        });
+    }
 
 }
 
