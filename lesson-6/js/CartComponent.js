f30438f17ab24a47e2a57bb140d0c555eda506da
ncowboy <latentpickuper@gmail.com>
2019-04-18 00:24:48 +0300

    sdfsfd

---
+++ lesson-6/js/CartComponent.js
@@ -0,0 +1,118 @@
+Vue.component('cart', {
+    data() {
+        return {
+            productsInCart: [],
+            getProductsInCartUrl: 'getBasket.json',
+            addToCartUrl: 'addToBasket.json',
+            deleteFromCartUrl: 'deleteFromBasket.json',
+            img: this.$parent.defaultImg,
+            showCart: false
+        }
+    },
+
+    mounted() {
+        this.$parent.request(this.getProductsInCartUrl)
+            .then(data => {
+                for (let el of data['contents']) {
+                    this.productsInCart.push(el);
+                }
+            });
+    },
+
+    methods: {
+        addToCart(product) {
+            this.$parent.request(this.addToCartUrl)
+                .then(data => {
+                    if (data.result) {
+                        const object = this.findProductInCart(product.id_product);
+                        if (object) {
+                            object.quantity++;
+                        } else {
+                            let newProduct = Object.assign({quantity: 1}, product);
+                            this.productsInCart.push(newProduct);
+                        }
+                    }
+                });
+        },
+
+        deleteFromCart(product) {
+            this.$parent.request(this.addToCartUrl)
+                .then(data => {
+                    if (data.result) {
+                        if (product.quantity > 1) {
+                            product.quantity--;
+                        } else {
+                            const index = this.productsInCart.indexOf(product);
+                            this.productsInCart.splice(index, 1);
+                        }
+                    }
+                });
+
+        },
+
+        findProductInCart(productId) {
+            const index = this.productsInCart.findIndex(product => product.id_product === productId);
+            if (index !== -1) {
+                return this.productsInCart[index];
+            } else {
+                return false;
+            }
+        },
+
+    },
+
+    computed: {
+        calculateCartQuantity() {
+            let sum = 0;
+            if (this.productsInCart.length !== 0) {
+                return this.productsInCart.reduce((accumulator, value = 0) => {
+                    return accumulator + value.quantity;
+                }, sum);
+            }
+        },
+
+        calculateCartSum() {
+            let sum = 0;
+            if (this.productsInCart.length !== 0) {
+                return this.productsInCart.reduce((accumulator, value = 0) => {
+                    return accumulator + value.quantity * value.price;
+                }, sum);
+            }
+        },
+
+    },
+
+    template: `<div>
+                <button class="btn btn-success ml-3" type="button"
+                  @click="showCart = !showCart">
+                  <i class="fas fa-shopping-cart"></i> 
+                  {{ calculateCartQuantity ? 'Товаров: ' + calculateCartQuantity 
+                      + ' на сумму: ' + calculateCartSum : 'Корзина пуста'}}
+                </button>
+                <div class="cart-content" v-show="showCart">
+                  <cart-item 
+                   v-for="product in productsInCart"
+                   :key="product.id_product"
+                   :data-id="product.id_product" 
+                   :img="img"
+                   :cart-item="product"
+                  ></cart-item> 
+                </div>
+              </div>`
+});
+
+Vue.component('cart-item', {
+    props: ['cartItem', 'img'],
+    template: `<div class="cart-item">
+                <div class="cart-item-img">
+                  <img :src="img" :alt="cartItem.product_name">
+                </div>
+                <div class="cart-item-name">{{ cartItem.product_name }}</div>
+                <div class="cart-item-qty">{{ cartItem.quantity }}</div>
+                <div class="cart-item-sum">{{ cartItem.price * cartItem.quantity }}</div>
+                <a @click.prevent="$parent.deleteFromCart(cartItem)" href="#" class="cart-item-delete" 
+                  :data-id="cartItem.id_product">
+                  <i class="fas fa-times-circle"></i>
+                </a>
+              </div>`,
+});
--- lesson-6/js/CatalogComponent.js
+++ lesson-6/js/CatalogComponent.js
@@ -1,49 +1,49 @@
 Vue.component('catalog', {
-  data() {
-    return {
-      products: [],
-      requestUrl: 'catalogData.json',
-      img: this.$parent.defaultImg
-    }
-  },
-
-  mounted() {
-    this.$parent.request(this.requestUrl)
-      .then(data => {
-        for (let el of data) {
-          this.products.push(el);
+    data() {
+        return {
+            products: [],
+            requestUrl: 'catalogData.json',
+            img: this.$parent.defaultImg
         }
-      });
-  },
-  template: `<div class="products row">
+    },
+
+    mounted() {
+        this.$parent.request(this.requestUrl)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+    },
+    template: `<div class="products row">
                 <catalog-item 
                    v-for="product in products"
                    :key="product.id_product" 
                    :data-id="product.id_product" 
-                   :catalog-item="product"
-                   :img="img"
+                   :item="product"
+                   :img="img">
                  </catalog-item>
               </div>`,
 
 });
 Vue.component('catalog-item', {
-  props: ['item', 'img'],
-  template: `<div class="product-item col-lg-3 col-md-6 mb-4"
-                <!--v-if="!isFilterOn || isFounded(product)">-->
+    props: ['item', 'img'],
+    template: `<div class="product-item col-lg-3 col-md-6 mb-4"
+                v-if="!$root.$refs.search.isFilterOn || $root.$refs.search.isFounded(item)">
                   <div class="card h-100">
                     <img 
                       :src="img" 
-                      :alt="product.product_name" class="card-img-top">
+                      :alt="item.product_name" class="card-img-top">
                     <div class="card-body d-flex flex-column justify-content-between">
-                      <h5 class="card-title">{{ product.product_name }}</h5>
-                      <p class="card-text">Цена: {{ product.price }} р.</p>
+                      <h5 class="card-title">{{ item.product_name }}</h5>
+                      <p class="card-text">Цена: {{ item.price }} р.</p>
                       <button class="cart-button btn btn-primary" type="submit"
-                        @click="addToCart(product)"  
-                         :data-id="product.id_product">
+                        @click="$root.$refs.cart.addToCart(item)"  
+                         :data-id="item.id_product">
                         <i class="fas fa-cart-arrow-down"></i> Добавить
                       </button>
                     </div>
                  </div>
-               </div>
+              </div>
             `,
 });
